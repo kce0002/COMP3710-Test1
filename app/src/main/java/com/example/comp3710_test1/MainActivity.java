bce2b56ee4764ffe4299529dc0614f5c1985e7dd
Kyle Ehlers <kce0002@auburn.edu>
2019-03-07 11:59:16 -0600

    Minor changes

--- app/src/main/java/com/example/comp3710_test1/MainActivity.java
+++ app/src/main/java/com/example/comp3710_test1/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.comp3710_test1;
 
-import android.content.Context;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
